4e70e99944234e603638d0688bab44090e89abc4
Bechir <bechir.ousmane.etu@univ-lille.fr>
2024-06-23 14:45:41 +0200

    Test hooks

--- Optimisation Itiniraire/project/app.py
+++ Optimisation Itiniraire/project/app.py
@@ -5,6 +5,7 @@ from api.data_base.src.dbApi import DBApi
 from datetime import datetime 
 
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
